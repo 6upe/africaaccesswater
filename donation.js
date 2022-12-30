cfb0b99e323b74afc8431908d0aeec2702d3b737
bupe <katongobupe444@gmail.com>
2022-12-30 01:14:24 -0800

    Website Update ----------------- 1. Projects Nav-link drop down to only items 2. Customized Pages (News, Projects, What_we_do, Reports) 3. Removed COVID 19 emergency relief section 4. Quick and popular linked to pages 5. Reports page (Front-end Only done) ------------------ visit page @ https://africaaccesswater.vercel.app/

---
+++ donation.js
@@ -0,0 +1,26 @@
+function donate(){
+    let amount = document.forms["donateForm"]["amount"].value;
+    let currency = document.forms["donateForm"]["currency"].value;
+    let name = document.forms["donateForm"]["name"].value;
+    let email = document.forms["donateForm"]["email"].value;
+    let message = document.forms["donateForm"]["message"].value;
+
+    switch (amount){
+        case '1':
+            window.document.location.href = "https://dashboard.flutterwave.com/donate/1lmbfjrz5bto";
+            break;
+        case '2':
+            window.document.location.href = "https://dashboard.flutterwave.com/donate/ybft0annqwzd";
+            break;
+        case '3':
+            window.document.location.href = "https://dashboard.flutterwave.com/donate/pntfbacoiei4";
+            break;
+        case '4':
+            window.document.location.href = "https://dashboard.flutterwave.com/donate/fbuza1huqnr8";
+            break;
+        default:
+             window.document.location.href = "index.html";
+             break;
+    }
+
+}

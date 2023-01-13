04e266498b9a52dc9a4c9f6feabcc924ad6db185
bupe <katongobupe444@gmail.com>
2023-01-13 06:44:13 -0800

    off canvas and donate form

--- donation.js
+++ donation.js
@@ -1,26 +1,41 @@
 function donate(){
     let amount = document.forms["donateForm"]["amount"].value;
     let currency = document.forms["donateForm"]["currency"].value;
-    let name = document.forms["donateForm"]["name"].value;
-    let email = document.forms["donateForm"]["email"].value;
-    let message = document.forms["donateForm"]["message"].value;
+    // let name = document.forms["donateForm"]["name"].value;
+    // let email = document.forms["donateForm"]["email"].value;
+    // let message = document.forms["donateForm"]["message"].value;
 
-    switch (amount){
+    if(currency == 1){
+        currency = "ZMW";
+    } else{
+        currency = "USD";
+    }
+
+    switch(amount){
         case '1':
+            amount = 20.00;
+            alert("Amount Selected: " + currency + " " + amount);
             window.document.location.href = "https://dashboard.flutterwave.com/donate/1lmbfjrz5bto";
             break;
         case '2':
+            amount = 50.00;
+            alert("Amount Selected: " + currency + " " + amount);
             window.document.location.href = "https://dashboard.flutterwave.com/donate/ybft0annqwzd";
             break;
         case '3':
+            amount = 100.00;
+            alert("Amount Selected: " + currency + " " + amount);
             window.document.location.href = "https://dashboard.flutterwave.com/donate/pntfbacoiei4";
             break;
         case '4':
+            alert("redirecting to another pages");
             window.document.location.href = "https://dashboard.flutterwave.com/donate/fbuza1huqnr8";
             break;
         default:
-             window.document.location.href = "#";
+             alert("unknown selection: ");
              break;
     }
+
+    // alert("Amount Selected: " +  amount);
 
 }

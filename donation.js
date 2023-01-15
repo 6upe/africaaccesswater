60cdcaaeeac6765fb2055bc9e0a20202d9ae62f6
bupe <katongobupe444@gmail.com>
2023-01-15 02:22:18 -0800

    Send msg function and setTimeout function

--- donation.js
+++ donation.js
@@ -1,9 +1,9 @@
 function donate(){
     let amount = document.forms["donateForm"]["amount"].value;
     let currency = document.forms["donateForm"]["currency"].value;
-    // let name = document.forms["donateForm"]["name"].value;
-    // let email = document.forms["donateForm"]["email"].value;
-    // let message = document.forms["donateForm"]["message"].value;
+    let user_name = document.forms["donateForm"]["name"].value;
+    let email = document.forms["donateForm"]["email"].value;
+    let message = document.forms["donateForm"]["message"].value;
 
     if(currency == 1){
         currency = "ZMW";
@@ -14,21 +14,21 @@ function donate(){
     switch(amount){
         case '1':
             amount = 20.00;
-            alert("Amount Selected: " + currency + " " + amount);
+            // alert("Amount Selected: " + currency + " " + amount);
             window.document.location.href = "https://dashboard.flutterwave.com/donate/1lmbfjrz5bto";
             break;
         case '2':
             amount = 50.00;
-            alert("Amount Selected: " + currency + " " + amount);
+            // alert("Amount Selected: " + currency + " " + amount);
             window.document.location.href = "https://dashboard.flutterwave.com/donate/ybft0annqwzd";
             break;
         case '3':
             amount = 100.00;
-            alert("Amount Selected: " + currency + " " + amount);
+            // alert("Amount Selected: " + currency + " " + amount);
             window.document.location.href = "https://dashboard.flutterwave.com/donate/pntfbacoiei4";
             break;
         case '4':
-            alert("redirecting to another pages");
+            // alert("redirecting to another pages");
             window.document.location.href = "https://dashboard.flutterwave.com/donate/fbuza1huqnr8";
             break;
         default:
@@ -36,6 +36,14 @@ function donate(){
              break;
     }
 
-    // alert("Amount Selected: " +  amount);
 
+    setTimeout(SendMessage(amount, currency, user_name, email, message),5000);
+    // SendMessage(amount, currency, user_name, email, message);
+    
+}
+
+function SendMessage(amount, currency, user_name, email, message){
+    let msg = 'Name: ' + user_name + ' Email: ' + email + ' Donation: ' + currency + amount + ' Message from Donor ' + message;
+
+        window.location.href = "https://wa.me/260962893773?text=" + msg;
 }
